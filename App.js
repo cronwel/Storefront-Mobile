0e3f9791cb899e9b56ec0a5272c295caf73f4aa7
Noel Irias <noelirias@gmail.com>
2021-07-15 15:44:43 -0500

    New Screens

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import HomeScreen from "./src/screens/HomeScreen";
-
+import { createAppContainer } from "react-navigation"
+import { createStackNavigator } from "react-navigation-stack"
+import HomeScreen from "./src/screens/HomeScreen"
+import ComponentsScreen from "./src/screens/ComponentsScreen"
+import LoginScreen from "./src/screens/LoginScreen"
+import JunkScreen from "./src/screens/JunkScreen"
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Component1: ComponentsScreen,
+    Login: LoginScreen,
+    Junk: JunkScreen,
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "Junk",
     defaultNavigationOptions: {
       title: "App",
+      profile: "ComponentsScreen",
     },
   }
-);
+)
 
-export default createAppContainer(navigator);
+export default createAppContainer(navigator)
